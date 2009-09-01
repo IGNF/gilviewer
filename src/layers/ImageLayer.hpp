394a8871ba1c5995e860860bd8fe103019aa064b
Adrien Chauve <petraike@gmail.com>
2009-09-01 17:20:28 +0200

    Link pb in derived application based on GilViewerLib

--- src/layers/ImageLayer.hpp
+++ src/layers/ImageLayer.hpp
@@ -59,8 +59,8 @@ knowledge of the CeCILL-B license and that you accept its terms.
 #include <boost/shared_ptr.hpp>
 #include <boost/mpl/vector.hpp>
 
-#include "gil/extension/matis/float_images.hpp"
-#include <boost/gil/extension/dynamic_image/any_image.hpp>
+#include <boost/gil/extension/matis/float_images.hpp>
+#include <boost/gil/extension/io/tiff_dynamic_io.hpp>
 
 #include "layers/Layer.hpp"
 
