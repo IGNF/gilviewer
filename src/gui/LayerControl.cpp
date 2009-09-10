b18768b68b4c49987a8054bec0ee512dd9b9e842
Adrien Chauve <petraike@gmail.com>
2009-09-10 19:27:20 +0200

    Workaround for compiling on windows/debug

--- src/gui/LayerControl.cpp
+++ src/gui/LayerControl.cpp
@@ -110,6 +110,27 @@ knowledge of the CeCILL-B license and that you accept its terms.
 
 extern void InitXmlResource();
 
+
+#ifdef _WINDOWS
+/* 
+
+	wx bug on windows MSVC (debug configuration)
+	tip found on: http://onegazhang.spaces.live.com/Blog/cns!D5E642BC862BA286!480.entry
+
+*/
+#include <wx/apptrait.h>
+#if wxUSE_STACKWALKER && defined( __WXDEBUG__ )
+// silly workaround for the link error with debug configuration:
+// \src\common\appbase.cpp
+wxString wxAppTraitsBase::GetAssertStackTrace()
+{
+   return wxT("");
+}
+#endif
+#endif
+
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///                                                                                                  ///
 ///                                 SelectableStaticText                                             ///
