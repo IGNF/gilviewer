847a2aa6df061d2ff01c4ed46bac3be920629107
Adrien Chauve <petraike@gmail.com>
2009-09-11 20:43:36 +0200

    Commit again workaraound; and don't generate panelviewer toolbar for windows

--- src/gui/LayerControl.cpp
+++ src/gui/LayerControl.cpp
@@ -101,7 +101,22 @@ knowledge of the CeCILL-B license and that you accept its terms.
 #include "resources/polygon_icon.xpm"
 #include "resources/image_icon.xpm"
 
+#ifdef WIN32
+#	include <wx/msw/winundef.h>
 
+/* seen on http://onegazhang.spaces.live.com/Blog/cns!D5E642BC862BA286!480.entry */
+#include <wx/apptrait.h>
+#if wxUSE_STACKWALKER && defined( __WXDEBUG__ )
+// silly workaround for the link error with debug configuration:
+// \src\common\appbase.cpp
+wxString wxAppTraitsBase::GetAssertStackTrace()
+{
+   return wxT("");
+}
+#endif
+
+
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///                                                                                                  ///
