832df1b53d9b7014c4cd9ef37fa2954f4668c6d7
Olivier Tournaire <olitour@gmail.com>
2009-10-14 11:09:15 +0200

    Compilation windows

--- src/gui/LayerControl.cpp
+++ src/gui/LayerControl.cpp
@@ -6,20 +6,20 @@ GilViewer is an open source 2D viewer (raster and vector) based on Boost
 GIL and wxWidgets.
 
 
-Homepage: 
+Homepage:
 
 	http://code.google.com/p/gilviewer
-	
+
 Copyright:
-	
+
 	Institut Geographique National (2009)
 
-Authors: 
+Authors:
 
 	Olivier Tournaire, Adrien Chauve
 
-	
-	
+
+
 
     GilViewer is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
@@ -31,9 +31,9 @@ Authors:
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public 
+    You should have received a copy of the GNU Lesser General Public
     License along with GilViewer.  If not, see <http://www.gnu.org/licenses/>.
- 
+
 ***********************************************************************/
 
 #include "gui/LayerControl.hpp"
@@ -98,7 +98,6 @@ wxString wxAppTraitsBase::GetAssertStackTrace()
    return wxT("");
 }
 #endif
-
 
 #endif
 
