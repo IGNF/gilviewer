49effec9240930081cb0c9aba0b5557f66cd572b
Olivier Tournaire <Olivier Tournaire@localhost>
2009-10-30 13:43:22 +0100

    Windows compilation

--- cmake/GilViewer_build_external_lib.cmake
+++ cmake/GilViewer_build_external_lib.cmake
@@ -1,15 +1,19 @@
 ##################
 ### Build de la ShapeLib ###
 ##################
-# Option pour selectionner une compilation sous forme de bibliotheque statique ou dynamique
-OPTION(BUILD_SHAPELIB_SHARED "Build shapelib library shared." ON)
-
-# Verification du type de bibliotheque a construire
-IF(BUILD_SHAPELIB_SHARED)
-  SET(SHAPELIB_LIBRARY_TYPE SHARED)
-ELSE(BUILD_SHAPELIB_SHARED)
-  SET(SHAPELIB_LIBRARY_TYPE STATIC)
-ENDIF(BUILD_SHAPELIB_SHARED)
+if( UNIX )
+	# Option pour selectionner une compilation sous forme de bibliotheque statique ou dynamique
+	OPTION(BUILD_SHAPELIB_SHARED "Build shapelib library shared." ON)
+	# Verification du type de bibliotheque a construire
+	IF(BUILD_SHAPELIB_SHARED)
+		SET(SHAPELIB_LIBRARY_TYPE SHARED)
+	ELSE()
+		SET(SHAPELIB_LIBRARY_TYPE STATIC)
+	ENDIF()
+# Currently, dll are not supported on Windows platforms
+else()
+	set( SHAPELIB_LIBRARY_TYPE STATIC )
+endif()
 
 # On fixe une variable globale regroupant tous les fichiers source de la shapelib
 # tout les autre fichier *.c sont des applications example
@@ -21,7 +25,7 @@ SET( ALL_SHPLIB_SRC_FILES	${SRC_SHPLIB}/dbfopen.c
 
 SET( ALL_SHPLIB_INC_FILES  "${SRC_SHPLIB}/shapefil.h" )
 MESSAGE(STATUS " create ALL_SHPLIB_INC_FILES " ${ALL_SHPLIB_INC_FILES} )
-		   
+
 # On ajoute les repertoires d'include
 INCLUDE_DIRECTORIES( ${INCLUDE_DIRECTORIES} ${SRC_SHPLIB} )
 # Creation de la lib
@@ -34,16 +38,19 @@ ADD_LIBRARY( shapelib ${SHAPELIB_LIBRARY_TYPE} ${ALL_SHPLIB_SRC_FILES} ${ALL_SHP
 ##################
 ###   Build de TinyXML  ###
 ##################
-
-# Option pour selectionner une compilation sous forme de .lib ou de .dll
-OPTION(BUILD_TINYXML_SHARED "Build tinyxml library shared." ON)
-
-# Verification du type de bibliotheque a construire
-IF(BUILD_TINYXML_SHARED)
-  SET(TINYXML_LIBRARY_TYPE SHARED)
-ELSE(BUILD_TINYXML_SHARED)
-  SET(TINYXML_LIBRARY_TYPE STATIC)
-ENDIF(BUILD_TINYXML_SHARED)
+if( UNIX )
+	# Option pour selectionner une compilation sous forme de .lib ou de .dll
+	OPTION(BUILD_TINYXML_SHARED "Build tinyxml library shared." ON)
+	# Verification du type de bibliotheque a construire
+	IF(BUILD_TINYXML_SHARED)
+	SET(TINYXML_LIBRARY_TYPE SHARED)
+	ELSE()
+		SET(TINYXML_LIBRARY_TYPE STATIC)
+	ENDIF()
+else()
+# Currently, dll are not supported on Windows platforms
+	set( TINYXML_LIBRARY_TYPE STATIC )
+endif()
 
 # On fixe une variable globale regroupant tous les fichiers source de tinyxml
 SET( SRC_TINYXMLLIB extern/tinyxml )
@@ -58,6 +65,11 @@ SET( ALL_TINYXML_INC_FILES ${SRC_TINYXMLLIB}/tinystr.h
 # On ajoute les repertoires d'include
 INCLUDE_DIRECTORIES( ${SRC_TINYXMLLIB} )
 INCLUDE_DIRECTORIES( extern )
+
+# Currently, dll are not supported on Windows platforms
+if( WIN32 )
+	set( TINYXML_LIBRARY_TYPE STATIC )
+endif()
 
 # Creation de la lib
 ADD_LIBRARY( tinyxml ${TINYXML_LIBRARY_TYPE} ${ALL_TINYXML_SRC_FILES} ${ALL_TINYXML_INC_FILES})
