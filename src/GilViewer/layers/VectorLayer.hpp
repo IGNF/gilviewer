babec4e45eade3dd7c423fd79be8b3ea9f98afae
mathieub <mathieub@gmail.com>
2010-02-09 11:36:22 +0100

    fix: many half-pixel bugs

---
+++ src/GilViewer/layers/VectorLayer.hpp
@@ -0,0 +1,168 @@
+/***********************************************************************
+
+This file is part of the GilViewer project source files.
+
+GilViewer is an open source 2D viewer (raster and vector) based on Boost
+GIL and wxWidgets.
+
+
+Homepage: 
+
+	http://code.google.com/p/gilviewer
+	
+Copyright:
+	
+	Institut Geographique National (2009)
+
+Authors: 
+
+	Olivier Tournaire, Adrien Chauve
+
+	
+	
+
+    GilViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GilViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with GilViewer.  If not, see <http://www.gnu.org/licenses/>.
+ 
+***********************************************************************/
+
+#ifndef __VECTOR_LAYER_HPP__
+#define __VECTOR_LAYER_HPP__
+
+#include <map>
+
+#include <boost/bind.hpp>
+
+class VectorLayerContent;
+
+#include "GilViewer/gui/VectorLayerSettingsControl.hpp"
+
+//ShapeLib
+#include "shapelib/shapefil.h"
+
+#include "GilViewer/layers/Layer.hpp"
+
+class VectorLayer : public Layer
+{
+public:
+	/// Creation d'un calque "a la main"
+	/// @param layerName Le nom du layer dans le LayerControl
+	/// @param layerType Le type du layer parmi (cf. http://shapelib.maptools.org/shp_api.html, section "Shape Types")
+	/// @param flagPRJ Indique si l'on est dans un système projeté : -1 si oui, +1 sinon (par défaut)
+	/// @param flagDBF Indique si on souhaite gerer des attributs : true si oui, false sinon (par défaut) [NON GERE !!!]
+    VectorLayer(const std::string &layerName , int layerType , signed short flagPRJ = 1 , bool flagDBF = false );
+    /// Constructeur a partir d'un nom de calque et d'un fichier shapefile
+    /// @param layerName Le nom du calque
+    /// @param shapefileFileName Le chemin vers le fichier shapefile
+    VectorLayer(const std::string &layerName,const std::string &shapefileFileName);
+    /// Simplement pour construire un calque "foutoir" (voir l'implementation)
+    VectorLayer(const std::string &layerName , signed short flagPRJ = 1 , bool flagDBF = false );
+
+	~VectorLayer() {};
+
+    static ptrLayerType CreateVectorLayer(const std::string &layerName , int layerType , signed short flagPRJ = 1 , bool flagDBF = false );
+    static ptrLayerType CreateVectorLayer(const std::string &layerName , const std::string &fileName);
+    static ptrLayerType CreateVectorLayer(const std::string &layerName , signed short flagPRJ = 1 , bool flagDBF = false );
+
+    virtual void AddVectorLayerContent( const std::string &shapefileFileName );
+
+    /// Cette methode recupere les donnees d'affichage par defaut dans les parametres et les affecte au nouveau calque
+    virtual void SetDefaultDisplayParameters();
+
+    int Type() const { return m_layerType; }
+
+    virtual void AddPoint( double x , double y );
+    virtual void AddPoints( const std::vector<double> &x , const std::vector<double> &y );
+    virtual void AddText( double x , double y , const std::string &text, const wxColour &color = *wxBLACK);
+    virtual void AddLine( double x1 , double y1 , double x2 , double y2 );
+    virtual void AddPolyline( const std::vector<double> &x , const std::vector<double> &y );
+    virtual void AddPolygon( const std::vector<double> &x , const std::vector<double> &y );
+
+    // Pour le FunnyVectorLayer
+    virtual void AddCircle( double x , double y , double radius );
+    virtual void AddSpline( std::vector<std::pair<double,double> > points );
+    virtual void AddEllipse(double x_center, double y_center, double a, double b);
+    virtual void AddEllipse(double x_center, double y_center, double a, double b, double theta);
+
+    virtual void SetBrushVectorLayerMultiGeometries( const wxBrush &brush );
+    virtual void SetBrushVectorLayerMultiGeometries( unsigned char red , unsigned char green , unsigned char blue , int style , unsigned char alpha=wxALPHA_OPAQUE );
+    virtual void SetPenVectorLayerMultiGeometries( const wxPen &pen );
+    virtual void SetPenVectorLayerMultiGeometries( unsigned char red , unsigned char green , unsigned char blue , int width = 1 , int style = wxSOLID , unsigned char alpha=wxALPHA_OPAQUE );
+    // Fin FunnyVectorLayer
+
+	virtual void Draw(wxDC &dc, wxCoord x, wxCoord y, bool transparent) const;
+	virtual void Update(int width, int height) {};
+
+    void GetInfos();
+    virtual void Save(const std::string &name) const;
+
+    virtual void PointsColour( const wxColour &colour , bool update = true );
+    virtual wxColour PointsColour() const;
+    virtual void PointsWidth( unsigned int width , bool update = true );
+    virtual unsigned int PointsWidth() const;
+    virtual void SetPointsStyle( const wxColour &colour , unsigned int width , bool update = true );
+
+    virtual void LinesColour( const wxColour &colour , bool update = true );
+	virtual wxColour LinesColour() const;
+    virtual void LinesWidth( unsigned int width , bool update = true );
+	virtual unsigned int LinesWidth() const;
+    virtual void LinesPenStyle( int style , bool update = true );
+	virtual int LinesPenStyle() const;
+    virtual void SetLinesStyle( const wxColour &colour , unsigned int width , int style , bool update = true );
+
+	virtual void PolygonsRingsColour( const wxColour &colour , bool update = true );
+	virtual wxColour PolygonsRingsColour() const;
+	virtual void PolygonsInsideColour( const wxColour &colour , bool update = true );
+	virtual wxColour PolygonsInsideColour() const;
+	virtual void PolygonsRingsStyle( unsigned int style , bool update = true );
+	virtual unsigned int PolygonsRingsStyle() const;
+	virtual void PolygonsInsideStyle( unsigned int style , bool update = true );
+	virtual unsigned int PolygonsInsideStyle() const;
+	virtual void PolygonsRingsWidth( unsigned int width , bool update = true );
+	virtual unsigned int PolygonsRingsWidth() const;
+
+	// TODO
+	//virtual void TextsColour( const wxColour &colour );
+	virtual void TextsFont( const wxFont &font , bool update = true ) { m_textFont = font; if (update) notifyLayerSettingsControl_(); }
+	virtual const wxFont& TextsFont() const { return m_textFont; }
+	virtual void TextsVisibility( bool value , bool update = true ) { m_isTextVisible = value; if (update) notifyLayerSettingsControl_(); }
+	virtual bool TextsVisibility() const { return m_isTextVisible; }
+
+	boost::shared_ptr<VectorLayerContent> LayerContent() { return m_layerContent; }
+	inline bool IsEditable() const { return m_isEditable; }
+	inline void IsEditable( bool editable ) { m_isEditable = editable; }
+
+	virtual void Clear();
+
+	virtual std::string Filename() const;
+
+private:
+	void Init();
+
+    boost::shared_ptr<VectorLayerContent> m_layerContent;
+
+    bool m_isEditable;
+    bool m_isFromFile;
+
+    // Pour l'affichage de texte
+    bool m_isTextVisible;
+    wxFont m_textFont;
+    std::vector< std::pair<double,double> > m_textCoordinates;
+    std::vector< std::string > m_textValue;
+    std::vector< wxColour > m_textColour;
+
+    int m_layerType;
+};
+
+
+#endif // __VECTOR_LAYER_HPP__
---
+++ src/GilViewer/layers/VectorLayerContent.cpp
@@ -0,0 +1,187 @@
+/***********************************************************************
+
+This file is part of the GilViewer project source files.
+
+GilViewer is an open source 2D viewer (raster and vector) based on Boost
+GIL and wxWidgets.
+
+
+Homepage:
+
+	http://code.google.com/p/gilviewer
+
+Copyright:
+
+	Institut Geographique National (2009)
+
+Authors:
+
+	Olivier Tournaire, Adrien Chauve
+
+
+
+
+    GilViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GilViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with GilViewer.  If not, see <http://www.gnu.org/licenses/>.
+
+***********************************************************************/
+
+#include <sstream>
+#include <stdexcept>
+#include <fstream>
+
+#include <boost/filesystem.hpp>
+
+#include <wx/log.h>
+#include <wx/string.h>
+
+#include "GilViewer/layers/VectorLayerContent.hpp"
+
+#include "GilViewer/wrappers/WrapperShapelibDBF.hpp"
+
+VectorLayerContent::VectorLayerContent() :
+	m_SHPHandle(NULL), m_shapefileFileName(""), m_flagPRJ(IMAGE_COORDINATES), m_flagDBF(false), m_drawAttribute(0), m_numberOfEntities(0), m_shapeType(SHPT_NULL)
+{
+	m_minBound[0] = m_maxBound[0] = 0.;
+	m_minBound[1] = m_maxBound[1] = 0.;
+	m_minBound[2] = m_maxBound[2] = 0.;
+	m_minBound[3] = m_maxBound[3] = 0.;
+}
+
+void VectorLayerContent::FlagPRJ(signed short flagPRJ)
+{
+	// Le flag doit obligatoirement avoir la valeur +1 ou -1
+	if (flagPRJ == CARTOGRAPHIC_COORDINATES || flagPRJ == IMAGE_COORDINATES)
+		m_flagPRJ = flagPRJ;
+	else
+	{
+		std::ostringstream oss;
+		oss << "Bad flag value ! Value must be +1 (IMAGE_COORDINATES) or -1 (CARTOGRAPHIC_COORDINATES) !" << std::endl;
+		oss << "flagPRJ = " << flagPRJ << std::endl;
+		oss << "File : " << __FILE__ << std::endl;
+		oss << "Line : " << __LINE__ << std::endl;
+		oss << "Function : " << __FUNCTION__ << std::endl;
+		throw std::logic_error(oss.str());
+	}
+
+}
+
+void VectorLayerContent::ReadAttributes(const std::string &DBFfilename)
+{
+	wxString mes;
+	mes << wxT("Reading DBF file") << wxString(DBFfilename.c_str(), *wxConvCurrent);
+	::wxLogMessage(mes);
+
+	try
+	{
+		WrapperShapelibDBF fileDBF(DBFfilename);
+
+		int nbFields = fileDBF.GetFieldCount();
+		int nbRecords = fileDBF.GetRecordCount();
+		mes.Clear();
+		mes << nbFields << wxT(" fields in the file ...\n") << nbRecords << wxT(" records in the file ...");
+		wxLogMessage(mes);
+
+		unsigned int i, j;
+		for (j = 0; j < static_cast<unsigned int> (nbFields); ++j)
+		{
+			std::vector< wxString> attributeValues;
+			std::string fieldName = fileDBF.GetFieldName(j);
+			for (i = 0; i < static_cast<unsigned int> (nbRecords); ++i)
+				attributeValues.push_back( wxString(fileDBF.ReadAttributeAsString(i,j).c_str(),*wxConvCurrent) );
+
+			//m_attributesDBF.insert( std::make_pair< wxString , std::vector< wxString > >( std::string( fieldName ) , attributeValues ) );
+			m_dbfAttributesNames.push_back(wxString(fieldName.c_str(), *wxConvCurrent));
+			m_dbfAttributesValues.push_back(attributeValues);
+			wxString ml;
+			ml << attributeValues.size();
+			wxLogMessage(ml);
+		}
+	}
+	catch( const WrapperShapelibDBFException& e)
+	{
+		mes.Clear();
+		mes << wxString(e.what(),*wxConvCurrent);
+		wxLogMessage(mes);
+	}
+}
+
+std::string VectorLayerContent::ShapeTypeAsString() const
+{
+	switch (m_shapeType)
+	{
+	case SHPT_NULL:
+		return "Invalid or unset geometries";
+		break;
+	case SHPT_POINT:
+		return "SHPT_POINT";
+		break;
+	case SHPT_ARC:
+		return "SHPT_ARC";
+		break;
+	case SHPT_POLYGON:
+		return "SHPT_POLYGON";
+		break;
+	case SHPT_MULTIPOINT:
+		return "SHPT_MULTIPOINT";
+		break;
+	case SHPT_POINTZ:
+		return "SHPT_POINTZ";
+		break;
+	case SHPT_ARCZ:
+		return "SHPT_ARCZ";
+		break;
+	case SHPT_POLYGONZ:
+		return "SHPT_POLYGONZ";
+		break;
+	case SHPT_MULTIPOINTZ:
+		return "SHPT_MULTIPOINTZ";
+		break;
+	case SHPT_POINTM:
+		return "SHPT_POINTM";
+		break;
+	case SHPT_ARCM:
+		return "SHPT_ARCM";
+		break;
+	case SHPT_POLYGONM:
+		return "SHPT_POLYGONM";
+		break;
+	case SHPT_MULTIPOINTM:
+		return "SHPT_MULTIPOINTM";
+		break;
+	case SHPT_MULTIPATCH:
+		return "SHPT_MULTIPATCH";
+		break;
+	default:
+		return "Unknown geometries";
+	}
+}
+
+void VectorLayerContent::CreatePRJ()
+{
+	std::ofstream prj(boost::filesystem::change_extension(m_shapefileFileName, ".prj").string().c_str());
+	prj.close();
+}
+std::string VectorLayerContent::GetInfos() const
+{
+	std::ostringstream oss;
+	oss << "Number of entities = " << m_numberOfEntities << "\n";
+	oss << "Entities type = " << ShapeTypeAsString() << "\n";
+	oss << "Minimal bounds :\n";
+	oss << "      X = " << m_minBound[0] << "\n";
+	oss << "      Y = " << m_minBound[1] << "\n";
+	oss << "Maximal bounds :\n";
+	oss << "      X = " << m_maxBound[0] << "\n";
+	oss << "      Y = " << m_maxBound[1] << "\n";
+	return oss.str();
+}
