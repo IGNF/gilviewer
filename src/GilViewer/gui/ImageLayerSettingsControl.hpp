6c2c3495acb9a0c6be4fedb3a8bb8247b7a5b919
Olivier Tournaire <olitour@gmail.com>
2010-02-16 10:43:56 +0100

    1) Move xpm in cpp files (***layersettingscontrol) 2) Manage (!) vectorlayermultigeometries styles

---
+++ src/GilViewer/gui/ImageLayerSettingsControl.hpp
@@ -0,0 +1,164 @@
+/***********************************************************************
+
+This file is part of the GilViewer project source files.
+
+GilViewer is an open source 2D viewer (raster and vector) based on Boost
+GIL and wxWidgets.
+
+
+Homepage: 
+
+	http://code.google.com/p/gilviewer
+	
+Copyright:
+	
+	Institut Geographique National (2009)
+
+Authors: 
+
+	Olivier Tournaire, Adrien Chauve
+
+	
+	
+
+    GilViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GilViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with GilViewer.  If not, see <http://www.gnu.org/licenses/>.
+ 
+***********************************************************************/
+
+#ifndef __IMAGE_LAYER_SETTINGS_CONTROL_HPP__
+#define __IMAGE_LAYER_SETTINGS_CONTROL_HPP__
+
+#include <vector>
+#include <wx/panel.h>
+
+#include "GilViewer/gui/LayerSettingsControl.hpp"
+
+class HistogramPlotter;
+class LayerControl;
+class wxSlider;
+class wxTextCtrl;
+class wxCheckBox;
+class wxFilePickerCtrl;
+
+class ImageLayerSettingsControl : public LayerSettingsControl
+{
+public:
+	ImageLayerSettingsControl(unsigned int index, LayerControl *parent, wxWindowID id = wxID_ANY, const wxString& title = _("Image layer settings"),
+		long style = wxDEFAULT_FRAME_STYLE , const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize );
+
+	void OnScroll(wxScrollEvent &event);
+	void OnOKButton(wxCommandEvent &event);
+	void OnCancelButton(wxCommandEvent &event);
+	void OnApplyButton(wxCommandEvent &event);
+	void OnSize(wxSizeEvent &event);
+	void OnCloseWindow(wxCloseEvent& event);
+	void OnCheckAlphaRange(wxCommandEvent &event);
+	void OnCheckAlphaChannel(wxCommandEvent &event);
+	void OnGetFocus(wxFocusEvent &event);
+
+	wxSlider* AlphaSlider() const {return m_alphaSlider;}
+
+	LayerControl* GetLayerControl() { return m_parent; }
+	HistogramPlotter* GetHistogramPlotter() { return m_histogramPanel; }
+
+	// Cette methode permet de mettre a jour l'interface lorsque des changements sont fait a partir du code (changement de style, de couleur ...)
+	virtual void update();
+
+        virtual std::string get_info_button_name() const {return "APPLICATIONS-GRAPHICS_16x16";}
+        virtual const char** get_icon_xpm() const;
+
+//private:
+	//wxSlider *m_minimumSlider;
+	//wxSlider *m_maximumSlider;
+	wxSlider *m_alphaSlider;
+	//wxSlider *m_gammaSlider;
+
+	wxTextCtrl* m_textRedChannel;
+	wxTextCtrl* m_textRedMinimumIntensity;
+	wxTextCtrl* m_textRedMaximumIntensity;
+	wxTextCtrl* m_textRedGamma;
+	wxTextCtrl* m_textGreenChannel;
+	wxTextCtrl* m_textGreenMinimumIntensity;
+	wxTextCtrl* m_textGreenMaximumIntensity;
+	wxTextCtrl* m_textGreenGamma;
+	wxTextCtrl* m_textBlueChannel;
+	wxTextCtrl* m_textBlueMinimumIntensity;
+	wxTextCtrl* m_textBlueMaximumIntensity;
+	wxTextCtrl* m_textBlueGamma;
+	wxTextCtrl* m_textAlphaRangeMin;
+	wxTextCtrl* m_textAlphaRangeMax;
+	wxTextCtrl* m_textAlphaChannel;
+
+	wxCheckBox* m_checkAlphaRange;
+	wxCheckBox* m_checkAlphaChannel;
+
+	//Fichier de Color Lookup Table pour les images mono canal
+	wxFilePickerCtrl *m_filePicker_CLUT;
+
+	unsigned int m_nbComponents;
+
+	DECLARE_EVENT_TABLE();
+private:
+	HistogramPlotter *m_histogramPanel;
+
+	LayerControl *m_parent;
+
+	bool IsNumeric( const std::string& value );
+};
+
+class HistogramPlotter : public wxPanel
+{
+public:
+	HistogramPlotter(ImageLayerSettingsControl* parent, const unsigned int redChannel, const unsigned int greenChannel, const unsigned int blueChannel, wxWindowID id = wxID_ANY, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxTAB_TRAVERSAL);
+	virtual ~HistogramPlotter() {;}
+
+    inline std::vector< std::vector< double > > &Data() { return m_histogram; }
+    double &Min(){ return m_min; }
+    double &Max(){ return m_max; }
+
+	void OnPaint(wxPaintEvent &event);
+	void OnSize(wxSizeEvent &event);
+	void OnMouseMove(wxMouseEvent &event);
+	bool IsInit() { return m_isInit; }
+	void IsInit(const bool init) {m_isInit=init;}
+	void SetChannels(const unsigned int red, const unsigned int green, const unsigned int blue) { m_redChannel=red; m_greenChannel=green; m_blueChannel=blue; }
+
+	DECLARE_EVENT_TABLE();
+
+private:
+	ImageLayerSettingsControl* m_parent;
+	std::vector< std::vector< double > > m_histogram;
+	double m_min, m_max;
+	bool m_isInit;
+	unsigned int m_redChannel, m_greenChannel, m_blueChannel;
+
+};
+
+class ThreadHistogram : public wxThread
+{
+public:
+	ThreadHistogram(ImageLayerSettingsControl *parent);
+
+	// thread execution starts here
+	virtual void *Entry();
+
+	// called when the thread exits - whether it terminates normally or is
+	// stopped with Delete() (but not when it is Kill()ed!)
+	virtual void OnExit();
+
+public:
+	size_t   m_count;
+	ImageLayerSettingsControl *m_parent;
+};
+#endif // __IMAGE_LAYER_SETTINGS_CONTROL_HPP__
---
+++ src/GilViewer/layers/VectorLayerPoint.cpp
@@ -0,0 +1,187 @@
+/***********************************************************************
+
+This file is part of the GilViewer project source files.
+
+GilViewer is an open source 2D viewer (raster and vector) based on Boost
+GIL and wxWidgets.
+
+
+Homepage:
+
+	http://code.google.com/p/gilviewer
+
+Copyright:
+
+	Institut Geographique National (2009)
+
+Authors:
+
+	Olivier Tournaire, Adrien Chauve
+
+
+
+
+    GilViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GilViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with GilViewer.  If not, see <http://www.gnu.org/licenses/>.
+
+***********************************************************************/
+
+#include <boost/filesystem.hpp>
+
+#include <wx/dc.h>
+#include <wx/config.h>
+#include <wx/progdlg.h>
+
+#include "VectorLayerPoint.hpp"
+
+GenericVectorLayerPoint::GenericVectorLayerPoint() : VectorLayerContent() {}
+
+void GenericVectorLayerPoint::Draw(wxDC &dc, wxCoord x, wxCoord y, bool transparent, double zoomFactor, double translationX, double translationY, double resolution) const
+{
+	const double delta = 0.5 * resolution;
+        wxPen penColour(m_border_color,m_width);
+        dc.SetPen(penColour);
+	bool draw_text = ( FlagDBF() && m_drawAttribute > 0 );
+	for (unsigned int i=0;i<m_points.size();i++)
+	{
+		wxPoint p = FromLocal(
+			zoomFactor,translationX,translationY,delta,
+			m_points[i].first,m_points[i].second);
+		// On draw des lines pq c'est beaucoup plus rapide et qu'en plus ca permet de gerer l'epaisseur ...
+		dc.DrawLine(p,p);
+		//dc.DrawPoint(p);
+		if(draw_text) dc.DrawText( m_dbfAttributesValues[m_drawAttribute-1][i],p);
+	}
+}
+
+VectorLayerPoint::VectorLayerPoint(const SHPHandle &handle , const std::string &shapefileFileName)
+{
+	m_SHPHandle = handle;
+	//m_shapefileFileName = boost::filesystem::basename(shapefileFileName);
+	m_shapefileFileName = shapefileFileName;
+    // On construit le vecteur qui contient les points
+    if ( m_SHPHandle != NULL )
+    {
+        SHPGetInfo( m_SHPHandle , &m_numberOfEntities , &m_shapeType , m_minBound , m_maxBound );
+		unsigned int i;
+
+		wxProgressDialog *progress = new wxProgressDialog(_("Reading shapefile ..."),_("Reading ..."),m_numberOfEntities,NULL,wxPD_AUTO_HIDE|wxPD_ELAPSED_TIME|wxPD_ESTIMATED_TIME|wxPD_REMAINING_TIME);
+
+		wxString m;
+		m << _("File : ") << wxString(m_shapefileFileName.c_str(), *wxConvCurrent) << wxT("\n");
+		m << _("Reading ") << static_cast<unsigned int>(m_numberOfEntities) << _(" objects");
+
+        for (i=0;i<static_cast<unsigned int>(m_numberOfEntities);++i)
+		{
+			if (i%3000==0)
+				progress->Update(i, m);
+            SHPObject* obj = SHPReadObject( m_SHPHandle , i );
+            m_points.push_back( std::make_pair<double,double>(obj->padfX[0] , obj->padfY[0]) );
+        }
+		delete progress;
+    }
+}
+
+void VectorLayerPoint::Save( const std::string &name )
+{
+	m_SHPHandle = SHPCreate( name.c_str() , SHPT_POINT );
+	boost::filesystem::path full = boost::filesystem::system_complete(name);
+	m_shapefileFileName = full.string();
+	//Filename( full.string() );
+	m_drawAttribute = 0;
+
+	if ( m_flagPRJ == CARTOGRAPHIC_COORDINATES )
+	{
+		CreatePRJ();
+	}
+
+	unsigned int i;
+	for (i=0;i<m_points.size();++i)
+	{
+		SHPObject* object = SHPCreateSimpleObject( SHPT_POINT , 1 , &(m_points[i].first) , &(m_points[i].second) , NULL );
+		SHPComputeExtents( object );
+		SHPWriteObject( m_SHPHandle , -1 , object );
+	}
+	SHPClose( m_SHPHandle );
+}
+
+void VectorLayerPoint::AddPoint( double x , double y )
+{
+	m_points.push_back( std::make_pair(x,y) );
+	m_numberOfEntities++;
+}
+
+void VectorLayerPoint::AddPoints( const std::vector<double> &x , const std::vector<double> &y )
+{
+	if ( x.size() != y.size() )
+		return;
+	std::vector<double>::const_iterator itbx = x.begin(), itby = y.begin();
+	for(;itbx!=x.end();++itbx,++itby)
+		AddPoint(*itbx,*itby);
+}
+
+VectorLayerPointM::VectorLayerPointM(const SHPHandle &handle , const std::string &shapefileFileName)
+{
+	m_SHPHandle = handle;
+	//m_shapefileFileName = boost::filesystem::basename(shapefileFileName);
+	m_shapefileFileName = shapefileFileName;
+    // On construit le vecteur qui contient les points
+    if ( m_SHPHandle != NULL )
+    {
+        SHPGetInfo( m_SHPHandle , &m_numberOfEntities , &m_shapeType , m_minBound , m_maxBound );
+		unsigned int i;
+
+		wxProgressDialog *progress = new wxProgressDialog(_("Reading shapefile ..."),_("Reading ..."),m_numberOfEntities,NULL,wxPD_AUTO_HIDE|wxPD_ELAPSED_TIME|wxPD_ESTIMATED_TIME|wxPD_REMAINING_TIME);
+
+		wxString m;
+		m << _("File : ") << wxString(m_shapefileFileName.c_str(), *wxConvCurrent) << wxT("\n");
+		m << _("Reading ") << static_cast<unsigned int>(m_numberOfEntities) << _(" objects");
+        for (i=0;i<static_cast<unsigned int>(m_numberOfEntities);++i)
+		{
+			if (i%3000==0)
+				progress->Update(i, m);
+            SHPObject* obj = SHPReadObject( m_SHPHandle , i );
+            m_points.push_back( std::make_pair<double,double>(obj->padfX[0] , obj->padfY[0]) );
+            m_measures.push_back( obj->padfM[0] );
+        }
+		delete progress;
+    }
+}
+
+VectorLayerPointZ::VectorLayerPointZ(const SHPHandle &handle , const std::string &shapefileFileName)
+{
+	m_SHPHandle = handle;
+	//m_shapefileFileName = boost::filesystem::basename(shapefileFileName);
+	m_shapefileFileName = shapefileFileName;
+    // On construit le vecteur qui contient les points
+    if ( m_SHPHandle != NULL )
+    {
+        SHPGetInfo( m_SHPHandle , &m_numberOfEntities , &m_shapeType , m_minBound , m_maxBound );
+		unsigned int i;
+
+		wxProgressDialog *progress = new wxProgressDialog(_("Reading shapefile ..."),_("Reading ..."),m_numberOfEntities,NULL,wxPD_AUTO_HIDE|wxPD_ELAPSED_TIME|wxPD_ESTIMATED_TIME|wxPD_REMAINING_TIME);
+
+		wxString m;
+		m << _("File : ") << wxString(m_shapefileFileName.c_str(), *wxConvCurrent) << wxT("\n");
+		m << _("Reading ") << static_cast<unsigned int>(m_numberOfEntities) << _(" objects");
+        for (i=0;i<static_cast<unsigned int>(m_numberOfEntities);++i)
+		{
+			if (i%3000==0)
+				progress->Update(i, m);
+            SHPObject* obj = SHPReadObject( m_SHPHandle , i );
+            m_points.push_back( std::make_pair<double,double>(obj->padfX[0] , obj->padfY[0]) );
+            m_Z.push_back( obj->padfZ[0] );
+        }
+		delete progress;
+    }
+}
