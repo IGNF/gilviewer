9cc3161020b426f9ff24782e560dc4a5e49277cd
Olivier Tournaire <olitour@gmail.com>
2010-02-16 11:41:11 +0100

    Fix ug in vector layer factory/ctor

--- src/GilViewer/layers/VectorLayer.hpp
+++ src/GilViewer/layers/VectorLayer.hpp
@@ -58,12 +58,12 @@ public:
     /// @param shapefileFileName Le chemin vers le fichier shapefile
     VectorLayer(const std::string &layerName,const std::string &shapefileFileName);
     /// Simplement pour construire un calque "foutoir" (voir l'implementation)
-    VectorLayer(const std::string &layerName , signed short flagPRJ = 1 , bool flagDBF = false );
+    VectorLayer(const std::string &layerName , signed short flagPRJ , bool flagDBF);
 
 	virtual ~VectorLayer() {};
 
     static ptrLayerType CreateVectorLayer(const std::string &layerName , const std::string &fileName);
-    static ptrLayerType CreateVectorLayer(const std::string &layerName , signed short flagPRJ = 1 , bool flagDBF = false );
+    static ptrLayerType CreateVectorLayer(const std::string &layerName , signed short flagPRJ, bool flagDBF);
 
     virtual void AddVectorLayerContent( const std::string &shapefileFileName );
 
