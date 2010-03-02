15cbd3e4adfaef9360aa0d5cfceb61f060f375e9
Olivier Tournaire <olitour@gmail.com>
2010-03-02 11:20:26 +0100

    Clean VectorLayer

--- src/GilViewer/layers/VectorLayer.cpp
+++ src/GilViewer/layers/VectorLayer.cpp
@@ -36,83 +36,25 @@ Authors:
 
 ***********************************************************************/
 
-#include <boost/filesystem.hpp>
-
-#include <sstream>
-using namespace std;
-
-#include <wx/dc.h>
-#include <wx/log.h>
-#include <wx/config.h>
-
 #include "../layers/VectorLayer.hpp"
-#include "../gui/define_id.hpp"
-
-void VectorLayer::Init()
-{
-    m_isTextVisible = true;
-    m_textFont = wxNullFont;
-    build_infos();
-}
 
 void VectorLayer::SetDefaultDisplayParameters()
 {
-    wxConfigBase *pConfig = wxConfigBase::Get();
-
     /// Points
     int redPoint = 255, bluePoint = 0, greenPoint = 0;
     int widthPoint = 3;
-    if ( pConfig )
-    {
-        pConfig->Read(wxT("/Options/VectorLayerPoint/Color/Red"), &redPoint, 255);
-        pConfig->Read(wxT("/Options/VectorLayerPoint/Color/Green"), &greenPoint, 0);
-        pConfig->Read(wxT("/Options/VectorLayerPoint/Color/Blue"), &bluePoint, 0);
-        pConfig->Read(wxT("/Options/VectorLayerPoint/Width"), &widthPoint, 3);
-    }
-    //SetPointsStyle( wxColour(redPoint,greenPoint,bluePoint) , widthPoint );
-    /// Points
-
     /// Lignes
     int redLine = 255, blueLine = 0, greenLine = 0;
     int widthLine = 3;
     int styleLine = wxSOLID;
-    if ( pConfig )
-    {
-        pConfig->Read(wxT("/Options/VectorLayerLine/Color/Red"), &redLine, 255);
-        pConfig->Read(wxT("/Options/VectorLayerLine/Color/Green"), &greenLine, 0);
-        pConfig->Read(wxT("/Options/VectorLayerLine/Color/Blue"), &blueLine, 0);
-        pConfig->Read(wxT("/Options/VectorLayerLine/Width"), &widthLine, 3);
-        pConfig->Read(_T("/Options/VectorLayerLine/Style"), &styleLine, wxSOLID);
-    }
-    //SetLinesStyle( wxColour(redLine,greenLine,blueLine) , widthLine , styleLine );
-    /// Lignes
-
     /// Polygones
     int redRing = 255, blueRing = 0, greenRing = 0;
     int redShape = 255, blueShape = 0, greenShape = 0;
     int ringWidth = 1;
     int penStyle = wxSOLID;
     int brushStyle = wxSOLID;
-    if ( pConfig )
-    {
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Ring/Color/Red"), &redRing, 255);
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Ring/Color/Green"), &greenRing, 0);
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Ring/Color/Blue"), &blueRing, 0);
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Ring/Width"), &ringWidth, 1);
 
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Shape/Color/Red"), &redShape, 255);
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Shape/Color/Green"), &greenShape, 0);
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Shape/Color/Blue"), &blueShape, 0);
-
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Style/Pen"), &penStyle, wxSOLID);
-        pConfig->Read(wxT("/Options/VectorLayerPolygon/Style/Brush"), &brushStyle, wxSOLID);
-    }
-    //PolygonsRingsColour( wxColour(redRing,greenRing,blueRing) );
-    //PolygonsInsideColour( wxColour(redShape,greenShape,blueShape) );
-    //PolygonsRingsStyle( penStyle );
-    //PolygonsInsideStyle( brushStyle );
-    //PolygonsRingsWidth( ringWidth );
-    /// Polygones
+    m_isTextVisible = true;
 
     set_point_color(wxColour(redPoint,greenPoint,bluePoint));
     set_point_width(widthPoint);
@@ -128,56 +70,13 @@ void VectorLayer::SetDefaultDisplayParameters()
     set_polygon_inner_style(brushStyle);
 }
 
-VectorLayer::VectorLayer( const string &layerName , signed short flagPRJ , bool flagDBF ) :
-        m_isFromFile(false),
-        m_layertype(MULTI_GEOMETRIES_TYPE)
-{
-    /*
-    Name(layerName);
-
-    m_layerContent = boost::shared_ptr<VectorLayerContent>( new VectorLayerMultiGeometries() );
-
-    m_layerContent->FlagPRJ( flagPRJ );
-    m_layerContent->FlagDBF( flagDBF );
-    SetDefaultDisplayParameters();
-    notifyLayerSettingsControl_();
-    Init();
-    */
-}
-
-Layer::ptrLayerType VectorLayer::CreateVectorLayer( const string &layerName , signed short flagPRJ , bool flagDBF )
-{
-    Layer::ptrLayerType ptrLayer(new VectorLayer(layerName,flagPRJ,flagDBF));
-    ptrLayer->notifyLayerSettingsControl_();
-    ptrLayer->SetDefaultDisplayParameters();
-    return ptrLayer;
-}
-
-void VectorLayer::build_infos()
-{
-    //m_infos = m_layerContent->GetInfos();
-}
-
 void VectorLayer::Clear()
 {
+    using namespace std;
+
     m_textCoordinates.clear();
-    m_textValue.clear();
-}
-
-vector<string> VectorLayer::get_available_formats_extensions() const
-{
-    return vector<string>();
-}
-
-string VectorLayer::get_available_formats_wildcard() const
-{
-    ostringstream wildcard;
-    wildcard << "All supported files (*.shp)|*.shp;*.SHP";
-    wildcard << wxT("Shapefile (*.shp)|*.shp;*.SHP");
-    return wildcard.str();
-}
-
-LayerSettingsControl* VectorLayer::build_layer_settings_control(unsigned int index, LayerControl* parent)
-{
-    return new VectorLayerSettingsControl(index, parent);
+    m_textValue.      clear();
+    // deallocate memory
+    vector<pair<double,double> >().swap(m_textCoordinates);
+    vector<string              >().swap(m_textValue      );
 }
--- src/GilViewer/layers/VectorLayer.hpp
+++ src/GilViewer/layers/VectorLayer.hpp
@@ -49,41 +49,14 @@ Authors:
 class VectorLayer : public Layer
 {
 public:
-    /// Constructeur a partir d'un nom de calque et d'un fichier shapefile
-    /// @param layerName Le nom du calque
-    /// @param shapefileFileName Le chemin vers le fichier shapefile
-    //VectorLayer(const std::string &layerName,const std::string &shapefileFileName);
-    /// Simplement pour construire un calque "foutoir" (voir l'implementation)
-    VectorLayer(const std::string &layerName , signed short flagPRJ , bool flagDBF);
-
-    virtual ~VectorLayer() {};
-
-    //static ptrLayerType CreateVectorLayer(const std::string &layerName , const std::string &fileName);
-    static ptrLayerType CreateVectorLayer(const std::string &layerName , signed short flagPRJ = 1 , bool flagDBF = false );
-
     /// Cette methode recupere les donnees d'affichage par defaut dans les parametres et les affecte au nouveau calque
     virtual void SetDefaultDisplayParameters();
 
-    int Type() const { return m_layertype; }
-
-    virtual void AddPoint( double x , double y ) {}
-    virtual void AddPoints( const std::vector<double> &x , const std::vector<double> &y ) {}
-    virtual void AddText( double x , double y , const std::string &text, const wxColour &color = *wxBLACK) {}
-    virtual void AddLine( double x1 , double y1 , double x2 , double y2 ) {}
-    virtual void AddPolyline( const std::vector<double> &x , const std::vector<double> &y ) {}
-    virtual void AddPolygon( const std::vector<double> &x , const std::vector<double> &y ) {}
-
-    // Pour le VectorLayerMultiGeometries
-    virtual void AddCircle( double x , double y , double radius ) {}
-    virtual void AddSpline( std::vector<std::pair<double,double> > points ) {}
-    virtual void AddEllipse(double x_center, double y_center, double a, double b) {}
-    virtual void AddEllipse(double x_center, double y_center, double a, double b, double theta) {}
-    // Fin VectorLayerMultiGeometries
-
+    // Abstract interface
     virtual void Draw(wxDC &dc, wxCoord x, wxCoord y, bool transparent) const {}
     virtual void Update(int width, int height) {};
 
-    void build_infos();
+    // Accessors
     virtual std::string get_layer_type_as_string() const {return "Vector";}
 
     // TODO
@@ -92,26 +65,14 @@ public:
 
     virtual void Clear();
 
-    virtual std::vector<std::string> get_available_formats_extensions() const;
-    virtual std::string get_available_formats_wildcard() const;
-
-    virtual LayerSettingsControl* build_layer_settings_control(unsigned int index, LayerControl* parent);
-    virtual bool is_saveable() const {return true;}
-
 private:
-    void Init();
-
-    bool m_isFromFile;
-
-    // Pour l'affichage de texte
+    // Textx display
     bool m_isTextVisible;
-    wxFont m_textFont;
     std::vector< std::pair<double,double> > m_textCoordinates;
     std::vector< std::string > m_textValue;
-    std::vector< wxColour > m_textColour;
 
 protected:
-    int m_layertype;
+    VectorLayer() : m_isTextVisible(true) {}
 };
 
 
