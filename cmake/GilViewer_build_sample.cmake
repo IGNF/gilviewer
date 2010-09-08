16b68fa4df80318af0cddb90de3deb19eeab34cd
Olivier Tournaire <olitour@gmail.com>
2010-09-08 17:17:06 +0200

    Add a sample to demonstrate subimage layer reading

--- cmake/GilViewer_build_sample.cmake
+++ cmake/GilViewer_build_sample.cmake
@@ -12,10 +12,18 @@ if(UNIX)
                                              ./viewer_app/gilviewer_app.hpp
                                              ./viewer_app/gilviewer_frame.cpp
                                              ./viewer_app/gilviewer_frame.hpp )
-                add_executable( sample_vector_layer ./samples/sample_vector_layer/sample_vector_layer_viewer.cpp ./samples/sample_vector_layer/sample_vector_layer.cpp )
+                add_executable( sample_vector_layer ./samples/sample_vector_layer/sample_vector_layer_viewer.cpp
+                                                    ./samples/sample_vector_layer/sample_vector_layer_viewer.hpp
+                                                    ./samples/sample_vector_layer/sample_vector_layer.cpp
+                                                    ./samples/sample_vector_layer/sample_vector_layer.h )
+                add_executable( sample_subimage ./samples/sample_subimage/sample_subimage_viewer.cpp
+                                                ./samples/sample_subimage/sample_subimage_viewer.hpp
+                                                ./samples/sample_subimage/sample_subimage.cpp
+                                                ./samples/sample_subimage/sample_subimage.hpp )
         endif()
         target_link_libraries( GilViewerApp ${GILVIEWER_LINK_EXTERNAL_LIBRARIES} GilViewer )
         target_link_libraries( sample_vector_layer ${GILVIEWER_LINK_EXTERNAL_LIBRARIES} GilViewer )
+        target_link_libraries( sample_subimage ${GILVIEWER_LINK_EXTERNAL_LIBRARIES} GilViewer )
 endif()
 # Si c'est du windows, on lui dit de faire une application windows (et pas une console)
 # On en profite aussi pour ajouter les resources
@@ -26,6 +34,18 @@ if(WIN32)
                                        ./viewer_app/gilviewer_frame.cpp
                                        ./viewer_app/gilviewer_frame.hpp
                                        ./viewer_app/GilViewer.rc )
+    add_executable( sample_vector_layer WIN32 ./samples/sample_vector_layer/sample_vector_layer_viewer.cpp
+                                              ./samples/sample_vector_layer/sample_vector_layer_viewer.hpp
+                                              ./samples/sample_vector_layer/sample_vector_layer.cpp
+                                              ./samples/sample_vector_layer/sample_vector_layer.h
+                                              ./samples/sample_vector_layer/sample_vector_layer.rc )
+    add_executable( sample_subimage WIN32 ./samples/sample_subimage/sample_subimage_viewer.cpp
+                                          ./samples/sample_subimage/sample_subimage_viewer.hpp
+                                          ./samples/sample_subimage/sample_subimage.cpp
+                                          ./samples/sample_subimage/sample_subimage.hpp
+                                          ./samples/sample_subimage/sample_subimage.rc )
     # Comme c'est sous visual (a priori ...), il y a l'auto link de Boost, donc pas besoin d'ajouter les libs ...
     target_link_libraries( GilViewerApp ${wxWidgets_LIBRARIES} GilViewer tinyxml ${GDAL_LIBRARY} )
+    target_link_libraries( sample_vector_layer ${wxWidgets_LIBRARIES} GilViewer tinyxml ${GDAL_LIBRARY} )
+    target_link_libraries( sample_subimage ${wxWidgets_LIBRARIES} GilViewer tinyxml ${GDAL_LIBRARY} )
 endif()
