989d65fe65918e87d758d9a30365382096e71c41
Olivier Tournaire <olitour@gmail.com>
2011-02-22 20:32:19 +0100

    Handle some plugins stuffs with CMake for UNIX and WIN32 plaforms

--- cmake/GilViewer_build_plugins.cmake
+++ cmake/GilViewer_build_plugins.cmake
@@ -1,3 +1,15 @@
+set( GILVIEWER_PLUGINS_DIR ${PROJECT_BINARY_DIR} )
+
+if(UNIX)
+    set( PLUGINS_EXT "so" )
+    set( PLUGINS_PRE "lib" )
+elseif(WIN32)
+    set( PLUGINS_EXT "dll" )
+    set( PLUGINS_PRE "" )
+endif()
+
+configure_file( ${CMAKE_CURRENT_SOURCE_DIR}/src/GilViewer/config/config_plugins.hpp.cmake.in ${CMAKE_CURRENT_SOURCE_DIR}/src/GilViewer/config/config_plugins.hpp )
+
 set(sample_plugin_SRCS ./src/GilViewer/plugins/sample_plugin.hpp ./src/GilViewer/plugins/sample_plugin.cpp )
 
 add_library(sample_plugin SHARED ${sample_plugin_SRCS})
