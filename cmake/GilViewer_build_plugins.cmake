fbdaffb630cf01e52abe0182aed38a438b173dbb
Bertrand Cannelle <bertrand.cannelle@ign.fr>
2011-08-01 16:12:21 +0200

    merge with M Bredif

--- cmake/GilViewer_build_plugins.cmake
+++ cmake/GilViewer_build_plugins.cmake
@@ -15,3 +15,23 @@ set(rotate_image_plugin_SRCS ./src/GilViewer/plugins/sample_plugin.hpp ./src/Gil
 add_library(rotate_image_plugin SHARED ${rotate_image_plugin_SRCS})
 target_link_libraries(rotate_image_plugin GilViewer)
 
+
+set(bertrand_plugin_SRCS 
+    ./src/GilViewer/plugins/Bertrand_plugin.hpp 
+    ./src/GilViewer/plugins/Bertrand_plugin.cpp 
+    /home/bcannelle/Programmation/Developpements/SVN/Modele_Donnees/These/code/detectionciel/ImageBinaire.cpp
+    /home/bcannelle/Programmation/Developpements/SVN/Modele_Donnees/These/code/detectionciel/ImageBinaire.h
+    )
+
+add_library(bertrand_plugin SHARED ${bertrand_plugin_SRCS})
+target_link_libraries(bertrand_plugin GilViewer)
+
+
+
+set(ciel_plugin_SRCS 
+    ./src/GilViewer/plugins/ciel_plugin.hpp 
+    ./src/GilViewer/plugins/ciel_plugin.cpp 
+    )
+
+add_library(ciel_plugin SHARED ${ciel_plugin_SRCS})
+target_link_libraries(ciel_plugin GilViewer)
