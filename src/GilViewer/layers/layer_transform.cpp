51c561fbcafb0560e7161eacd8fa413f268a307b
Bertrand Cannelle <bertrand.cannelle@ign.fr>
2011-08-05 11:39:54 +0200

    add file layer_transform.cpp

---
+++ src/GilViewer/layers/layer_transform.cpp
@@ -0,0 +1,31 @@
+#include "layer_transform.hpp"
+
+wxRealPoint layer_transform::rotated_coordinate_to_local(const wxRealPoint& pt)const{
+        wxRealPoint out(pt);
+        if(m_h==0 && m_w==0)
+            return out;
+        switch (m_layer_orientation){
+        case LO_0: break;//return pt;//correct
+        case LO_90: out= wxRealPoint(pt.y,m_h-pt.x-1);break;//correct
+        case LO_180: out= wxRealPoint(m_w-pt.x-1.,m_h-pt.y-1.);break;//correct
+        case LO_270: out= wxRealPoint(m_w-pt.y-1,pt.x);break;//correct
+        }
+        //std::cout<<__FUNCTION__<<" "<<pt.x<<" "<<pt.y<<" "<<out.x<<" "<<out.y<<" "<<m_w<<" "<<m_h<<" "<<(unsigned int)m_layer_orientation<<std::endl;
+        //return pt;
+        return out;
+    }
+
+wxRealPoint  layer_transform::rotated_coordinate_from_local(const wxRealPoint& pt)const{
+    wxRealPoint out(pt);
+//        return out;
+        if(m_h==0 && m_w==0)
+            return out;
+        switch (m_layer_orientation){
+        case LO_0: break;//return pt;//correct
+        case LO_90: out=wxRealPoint(m_h-pt.y-1,pt.x);break;//return wxRealPoint(m_h-pt.y-1,pt.x);//correct
+        case LO_180: out=wxRealPoint(m_w-pt.x-1.,m_h-pt.y-1.);break;//return wxRealPoint(m_w-pt.x-1.,m_h-pt.y-1.);//correct
+        case LO_270: out=wxRealPoint(pt.y,m_w-pt.x-1.);break;//return wxRealPoint(pt.y,m_w-pt.x-1.);//correct
+        }
+        //std::cout<<__FUNCTION__<<" "<<pt.x<<" "<<pt.y<<" "<<out.x<<" "<<out.y<<" "<<m_w<<" "<<m_h<<" "<<(unsigned int)m_layer_orientation<<std::endl;
+        return out;
+    }
---
+++ src/GilViewer/layers/vector_layer_ghost.hpp
@@ -0,0 +1,111 @@
+/***********************************************************************
+
+This file is part of the GilViewer project source files.
+
+GilViewer is an open source 2D viewer (raster and vector) based on Boost
+GIL and wxWidgets.
+
+
+Homepage: 
+
+	http://code.google.com/p/gilviewer
+
+Copyright:
+
+	Institut Geographique National (2009)
+
+Authors: 
+
+	Olivier Tournaire, Adrien Chauve
+
+	
+	
+
+    GilViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GilViewer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public 
+    License along with GilViewer.  If not, see <http://www.gnu.org/licenses/>.
+
+***********************************************************************/
+
+#ifndef VECTORLAYERGENERIC_H_
+#define VECTORLAYERGENERIC_H_
+
+#include <utility>
+#include <vector>
+
+#include <wx/pen.h>
+#include <wx/brush.h>
+#include <wx/gdicmn.h>
+
+#include "layer_transform.hpp"
+#include "boost/variant.hpp"
+
+class wxDC;
+class wxPoint;
+
+class vector_layer_ghost
+{
+public:
+
+    // constructor
+    vector_layer_ghost();
+
+    // input types
+    struct Nothing {};
+    typedef wxRealPoint Point;
+    typedef std::pair<wxRealPoint,wxRealPoint> Rectangle;
+    typedef std::pair<wxRealPoint,double> Circle;
+    class Polyline : public std::vector<wxRealPoint> {};
+    class Polygon : public std::vector<wxRealPoint> {};
+    typedef boost::variant<Nothing,Point,Rectangle,Circle,Polyline,Polygon> variant_input;
+
+
+    // input reseting (same input type)
+    void reset();
+    // input reseting to a new given input type
+    template<typename T> void reset()
+    {
+        m_input = T();
+        m_complete = true;
+    }
+
+    // drawing and geometry modifications
+    void draw(wxDC &dc, wxCoord x, wxCoord y, bool transparent);
+    bool add_point(const wxRealPoint& p, bool final = false);
+    void move_relative(const wxRealPoint& p);
+    void move_absolute(const wxRealPoint& p);
+    void update_relative(const wxRealPoint& p);
+    void update_absolute(const wxRealPoint& p);
+
+    // rendering style
+    wxPen m_penPoint, m_penRectangle, m_penCircle, m_penLine;
+    wxBrush m_brushRectangle, m_brushCircle;
+
+    // transform
+    layer_transform& transform() { return m_layer_transform; }
+    const layer_transform& transform() const { return m_layer_transform; }
+
+    // input is ready for consumption
+    inline bool complete() const { return m_complete; }
+    inline void complete(bool c) { m_complete = c; }
+
+    // get the input
+    const variant_input& get() const { return m_input; }
+    template<typename T> inline const T* get() const { return boost::get<T>(&m_input); }
+
+private:
+    layer_transform m_layer_transform;
+    variant_input m_input;
+    bool m_complete;
+};
+
+#endif /*VECTORLAYERGENERIC_H_*/
