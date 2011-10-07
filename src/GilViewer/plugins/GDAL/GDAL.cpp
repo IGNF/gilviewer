57c38ac976d6b43706df2a309c7be25f989faf74
Olivier Tournaire <olitour@gmail.com>
2011-10-07 20:56:33 +0200

    fix missing OGRRegisterAll() for GDAL managed formats

--- src/GilViewer/plugins/GDAL/GDAL.cpp
+++ src/GilViewer/plugins/GDAL/GDAL.cpp
@@ -6,8 +6,11 @@ IMPLEMENT_PLUGIN(GDAL_plugin);
 #include "gilviewer_file_io_kml.hpp"
 #include "gilviewer_file_io_gdal_jp2.hpp"
 
+#include <gdal/ogrsf_frmts.h>
+
 bool GDAL_plugin::Register(gilviewer_io_factory *factory)
 {
+    OGRRegisterAll();
     gilviewer_file_io_shp     ().Register(factory);
     gilviewer_file_io_kml     ().Register(factory);
     gilviewer_file_io_gdal_jp2().Register(factory);
