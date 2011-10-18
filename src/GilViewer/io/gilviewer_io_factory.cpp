4326ea4c7fbf23f74091661da2f9c23a1874124a
Olivier Tournaire <olitour@gmail.com>
2011-10-18 18:43:02 +0200

    merge from stable

--- src/GilViewer/io/gilviewer_io_factory.cpp
+++ src/GilViewer/io/gilviewer_io_factory.cpp
@@ -9,6 +9,7 @@
 #include "gilviewer_file_io_serialization_binary.hpp"
 #include "gilviewer_file_io_serialization_txt.hpp"
 #include "gilviewer_file_io_serialization_xml.hpp"
+#include "gilviewer_file_io_cr2.hpp"
 
 void register_all_image_file_formats(gilviewer_io_factory *factory)
 {
@@ -17,6 +18,8 @@ void register_all_image_file_formats(gilviewer_io_factory *factory)
     gilviewer_file_io_png ().Register(factory);
     gilviewer_file_io_pnm ().Register(factory);
     gilviewer_file_io_tiff().Register(factory);
+    // TODO: move as a plugin?
+    gilviewer_file_io_cr2().Register(factory);
 }
 
 void register_all_vector_file_formats(gilviewer_io_factory *factory)
